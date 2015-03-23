5ba9ebf2424238143f556306f21c517ab6d5075d
Tim Cooper <coop@latrobest.com>
2015-03-23 15:57:56 +1000

    Specify syslog ip and port with env vars

--- loggle-wrap.sh
+++ loggle-wrap.sh
@@ -4,13 +4,10 @@ process_type="${PLAIN_PROCESS_TYPE}"
 app_name="${PLAIN_APP_NAME}"
 docker_container="`hostname`"
 
-syslog_server="$SYSLOG_PORT_5140_UDP_ADDR"
-syslog_port="$SYSLOG_PORT_5140_UDP_PORT"
-
 log_tag="${app_name}[${process_type}.${docker_container}]"
 
 if [ x"$syslog_server" = x ]; then
   "$@" | logger -t "$log_tag"
 else
-  "$@" | logger --udp -n "$syslog_server" -P "$syslog_port" -t "$log_tag"
+  "$@" | logger --udp -n "$SYSLOG_IP" -P "$SYSLOG_PORT" -t "$log_tag"
 fi
